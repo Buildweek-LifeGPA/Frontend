752a5e055075a62325f508909c79e615f9636f74
Hector Valdez <hvaldez180@gmail.com>
2019-09-24 18:30:55 -0400

    updated launch screen

--- src/components/LaunchScreen.js
+++ src/components/LaunchScreen.js
@@ -1,15 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
+
+const P = styled.p`
+
+`;
+
+const BtnContainer = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const CustomButton = styled.button`
+margin-top: 5%;
+margin-left: 10%;
+margin-right: 10%;
+padding: 3%;
+`;
+
+const CreateBtn = styled.button`
+background-color: yellow;
+margin-top: 5%;
+margin-left: 10%;
+margin-right: 10%;
+padding: 3%;
+`;
 
 export default function LaunchScreen () {
     return (
         <div>
            <img src="src/images/LifeGPALogo.png" alt="A logo of life GPA"/>
-           <p>Welcome to the new you!</p>
-           <div className="button-container">
-           <button className="create-btn">CREATE ACCOUNT</button>
-           <button>LOGIN IN</button>
-           </div>
+           <P>Welcome to the new you!</P>
+           <BtnContainer>
+           <CreateBtn>CREATE ACCOUNT</CreateBtn>
+           <CustomButton>LOGIN IN</CustomButton>
+           </BtnContainer>
        </div>
 
     );
